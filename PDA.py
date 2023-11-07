0225795f55ac7b61f1a6dd4ea063fa98f5535754
Gautam Yajaman <g.yajaman@gmail.com>
2023-11-07 11:15:40 +0530

    PDAs added

--- PDA.py
+++ PDA.py
@@ -1,6 +1,5 @@
 import re
-
-test = "0011111"
+import numpy as np
 
 currPos = 0
 
@@ -12,9 +11,11 @@ alphCount = 0
 alphToIndex = {}
 indexToAlph = []
 
-transCount = 0
+symbCount = 0
+symbToIndex = {}
+indexToSymb = []
 
-stack = ['Z0']
+transCount = 0
 
 accept = []
 
@@ -24,9 +25,9 @@ f.close()
 
 for line in temp:
     line = line.strip()
-    if line == 'states:' or line == 'start:' or line == 'alphabet:' or line == 'transitions:' or line == 'accept:':
+    if line == 'states:' or line == 'start:' or line == 'alphabet:' or line == 'transitions:' or line == 'accept:' or line == 'stack symbols:':
         if line == 'transitions:':
-            trans = [[None for j in range(alphCount)] for i in range(stateCount)]
+            trans = [[[[None,None] for j in range(alphCount)] for i in range(stateCount)] for k in range(symbCount)]
         currPos += 1 
         continue
     if currPos == 1:
@@ -35,63 +36,51 @@ for line in temp:
         stateCount += 1
     if currPos == 2:
         start = line
-    if currPos == 3:
+    elif currPos == 3:
         alphToIndex[line] = alphCount
         indexToAlph = indexToAlph + [line]
         alphCount += 1
-    if currPos == 4:
+    elif currPos == 4:
+        symbToIndex[line] = symbCount
+        indexToSymb = indexToSymb + [line]
+        symbCount +=1
+    elif currPos == 5:
         temp = re.split(">|,",line)
-        trans[statesToIndex[temp[0]]][alphToIndex[temp[1]]] = [temp[2]]+[temp[3]]+[temp[4]]
-    if currPos == 5:
+        trans[statesToIndex[temp[0]]][alphToIndex[temp[1]]][symbToIndex[temp[2]]] = [temp[3]]+[temp[4]]
+    else:
         accept = accept + [line]
 
-for element in indexToAlph:
-    print('\t'+element,end=' ')
-print()
-
-i = 0
-for row in trans:
-    print(indexToStates[i],end=' ')
-    for element in row:
-        print('\t'+str(element),end=' ')
-    i+=1
-    print() 
-
 while(True):
-    stack = ['Z0']
-    print()
     test = input("Enter a string:")
-    flag = False
+
     currentState = start
+    stack = 'Z'
+
     for i in range(len(test)):
-        if test[0] not in alphToIndex:
-            break
-        print(currentState+','+test+',',end='')
-        print(stack,end=' ')
-        print('->',end=' ')
-        if trans[statesToIndex[currentState]][alphToIndex[test[0]]][1] == 'pop': 
-            currentState = trans[statesToIndex[currentState]][alphToIndex[test[0]]][0]#move to new state specified by the transition table
-            if stack[len(stack)-1] == trans[statesToIndex[currentState]][alphToIndex[test[0]]][2]:
-                stack = stack[:len(stack)-1] #pop from stack
-            else:
-                break
-        else:
-            stack = stack + [trans[statesToIndex[currentState]][alphToIndex[test[0]]][2]] #push to stack
-            currentState = trans[statesToIndex[currentState]][alphToIndex[test[0]]][0] #move to state specifice by the transition table
-        test = test[1:]
-        if currentState == None:
-            print("REJECT")
-            flag = True
+        if test[0] not in indexToAlph:
             break
 
+        print(currentState+','+test+','+stack+' ->',end=' ')
+        action = trans[statesToIndex[currentState]][alphToIndex[test[0]]][symbToIndex[stack[0]]][1]
+        currentState = trans[statesToIndex[currentState]][alphToIndex[test[0]]][symbToIndex[stack[0]]][0]
+
+        if currentState == None:
+            break
+
+        if action == '$':
+            stack = stack[1:]
+        else:
+            stack = action+stack[1:]
+
+        test = test[1:]
+    
     if test == '':
         test = 'ε'
-    if not flag:
-        if stack == ['Z0'] and test == 'ε' and currentState in accept:
-            print(currentState+','+test+',',end='')
-            print(stack,end=' ')
-            print('-> ACCEPT')
+    
+    if test == 'ε' and stack == 'Z' and currentState in accept:
+        print(currentState+','+test+','+stack+' -> ACCEPT')
+    else:
+        if currentState != None:
+            print(currentState+','+test+','+stack+' -> REJECT')
         else:
-            print(currentState+','+test+',',end='')
-            print(stack,end=' ')
-            print("-> REJECT")
+            print('REJECT')
