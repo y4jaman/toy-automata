e9ea7e2831a7dec9b9e44f6ea13067383876d2dc
Gautam Yajaman <g.yajaman@gmail.com>
2023-11-10 19:47:50 +0530

    Printing format fixed along with edge cases

--- Turing.py
+++ Turing.py
@@ -1,13 +1,42 @@
 import re
 import os
+import time
 
 def refactorLeft(tape):
-    tape = ['B']*10 + tape
+    tape = ['B']*3 + tape
     return tape
 
 def refactorRight(tape):
-    tape = tape + ['B']*10
+    tape = tape + ['B']*3
     return tape
+
+def print_tape(list, index_ptr):
+    # Function to create a box around the element
+    def box(element):
+        return f"+---+\n| {element} |\n+---+"
+
+    # Print the top part of the boxes
+    for element in list:
+        print("+---+\t", end='')
+    print()  # Move to the next line
+
+    # Print the elements with side borders
+    for element in list:
+        print(f"| {element} |\t", end='')
+    print()  # Move to the next line
+
+    # Print the bottom part of the boxes
+    for element in list:
+        print("+---+\t", end='')
+    print()  # Move to the next line
+
+    # Print the arrow under the element at index_ptr
+    for i in range(len(list)):
+        if i == index_ptr:
+            print("  ^  \t", end='')  # Align the arrow with the center of the box
+        else:
+            print("\t", end='')
+    print()  # Move to the next line
 
 currPos = 0
 
@@ -68,20 +97,18 @@ for line in temp:
 while(True):
     test = input("Enter a string:")
     currentState = start
-    ptr = 0
-    tape = [letter for letter in test]
+    ptr = 1
+    tape = ['B']+[letter for letter in test]+['B']
     while(True):
-        dummy = input()
-        os.system('cls')
+        time.sleep(0.3)
         if currentState == None:
             print("REJECT")
             break
         if currentState == accept:
             print("ACCEPT")
             break
-        print(tape)
-        print(currentState)
-        print(ptr)
+        os.system('cls')
+        print_tape(tape,ptr)
         next = trans[statesToIndex[currentState]][alphToIndex[tape[ptr]]]
         tape[ptr] = next[1]
         currentState = next[0]
